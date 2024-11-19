9984ae7f3abaf8bbc1fb29c5252d59b7bdf8430e
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-18 19:35:15 -0500

    greaterthanlessthan

---
+++ greaterthanlessthan.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a number :"))
+if (x < 20):
+    print ("x is smaller than 20")
+    print ("i'm in if Block")
+ 
+else:
+    print ("x is greater than 20")
+    print ("i'm in else Block")
+print ("i'm not in if and not in else Block")
